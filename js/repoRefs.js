fb83c82896f1c5fcccfd0777ea0380f13e5222cc
Vakaliuk <69085662+Vakaliuk@users.noreply.github.com>
2023-04-25 20:46:06 +0300

    added class to repo load + token

--- js/repoRefs.js
+++ js/repoRefs.js
@@ -1,24 +1,42 @@
-const reposRef = document.querySelector('.repos');
+const gitToken = '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';
+const user = 'Ihor';
 
-function fetchRepo() {
-  fetch('https://api.github.com/users/Vakaliuk/repos')
-    .then((Response) => Response.json())
-    .then((json) => {
-      json.forEach((item) => {
-        const repo = document.createElement('li');
-        repo.innerHTML = `<a href="${item.html_url}"> ${item.full_name} </a>`;
-        repo.classList.add('repo-item');
+const headers = {
+  Authorization: `Bearer ${gitToken}`,
+  'Content-Type': 'application/json',
+};
 
-        const repoDesc = document.createElement('p');
-        repoDesc.textContent = `- ${item.description}`;
-        repoDesc.classList.add('repo-description');
+class gitAPI {
+  constructor(name, token) {
+    this.name = name;
+    this.token = token;
+  }
 
-        reposRef.appendChild(repo);
-        if (repoDesc.textContent !== '- null') {
-          repo.appendChild(repoDesc);
-        }
-      });
+  async getRepos() {
+    const reposRef = document.querySelector('.repos');
+
+    const reposRaw = await fetch(
+      'https://api.github.com/users/Vakaliuk/repos',
+      {
+        method: 'GET',
+        headers: headers,
+      }
+    );
+    const repos = await reposRaw.json();
+
+    repos.forEach((item) => {
+      const repo = document.createElement('li');
+      repo.innerHTML = `<a href="${item.html_url}"> ${item.full_name} </a>`;
+      repo.classList.add('repo-item');
+
+      const repoDesc = document.createElement('p');
+      repoDesc.textContent = `- ${item.description}`;
+      repoDesc.classList.add('repo-description');
+
+      reposRef.appendChild(repo);
+      if (repoDesc.textContent !== '- null') {
+        repo.appendChild(repoDesc);
+      }
     });
+  }
 }
-
-fetchRepo();
